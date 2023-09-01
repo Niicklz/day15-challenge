69917527ada028f10b074c64a01aa80dc021299e
Jose Vasquez <josevasquez8365@gmail.com>
2023-09-01 13:46:23 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll(
+  "[data-counter]"
+) as NodeListOf<HTMLHeadingElement>;
+console.log();
+
+const incrementFollowers: () => void = () => {
+  const followers:Array<{name:string,maxFollowers:number}> = [
+    { name: "twitter", maxFollowers: 12000 },
+    { name: "youtube", maxFollowers: 5000 },
+    { name: "facebook", maxFollowers: 7500 },
+  ];
+counters.forEach((counterEl,index) => {
+  let counterFollows = 0
+ const counterInterval = setInterval(()=> {
+counterEl.textContent  = counterFollows.toString()
+counterFollows += 100
+if(counterFollows > followers[index].maxFollowers) clearInterval(counterInterval)
+  },0.3)
+  
+})
+};
+incrementFollowers()
